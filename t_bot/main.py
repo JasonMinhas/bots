a1318bfce0036f63caef7b948be88bbdf9cd2074
Jason Minhas <62856363+JasonMinhas@users.noreply.github.com>
2021-12-19 05:06:59 +0000

    added secrets

--- t_bot/main.py
+++ t_bot/main.py
@@ -9,7 +9,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
   channel = client.get_channel(741478408575385704)
-  await channel.send("Use the command '$[name]_fact' to get a fun fact about someone.")
+  await channel.send("Last 10 minutes of audio have been saved on the cloud.")
 
 @client.event
 async def on_message(message):
